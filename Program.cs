96af884dcaa000d81835bb1a6054ced9e5b5078b
Bozhidar <mrarthur0507@gmail.com>
2024-10-22 16:58:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Create and configure the Host for the worker
+using JudgeWorker;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        
+        services.AddMassTransit(x =>
+        {
+            
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host("localhost", "/", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                
+                cfg.ReceiveEndpoint("execute-code-queue", e =>
+                {
+                    e.ConfigureConsumer<ExecuteCodeConsumer>(context);
+                });
+            });
+
+            
+            x.AddConsumer<ExecuteCodeConsumer>();
+        });
+
+        
+        services.AddMassTransitHostedService();
+    })
+    .Build();
+
+await host.RunAsync();
